803738342600fc6feb1406056998534815673d20
zhaomingjie <zhaomingjie@kjtpay.com.cn>
2018-11-30 13:59:54 +0800

    Initial commit

---
+++ app/test/src/test/java/com/zmj/archetype/springboot/ArchetypeSpringbootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zmj.archetype.springboot;
+
+import com.zmj.archetype.springboot.web.ArchetypeSpringbootApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ArchetypeSpringbootApplication.class)
+public class ArchetypeSpringbootApplicationTests {
+
+    @Test
+    public void test(){
+
+    }
+}
---
+++ app/web/src/main/java/com/zmj/archetype/springboot/web/ArchetypeSpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.zmj.archetype.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhaomingjie
+ */
+@SpringBootApplication
+public class ArchetypeSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchetypeSpringbootApplication.class, args);
+    }
+}
